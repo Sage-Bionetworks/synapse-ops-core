7bb8d2bf992c5e7ed41868cc17cddf8b07fc8263
Xavier Schildwachter <x.schildwachter@sagebase.org>
2025-08-28 10:29:47 -0700

    Add support for instance

--- scripts/build_synapse_cdn_webacl.sh
+++ scripts/build_synapse_cdn_webacl.sh
@@ -5,16 +5,19 @@
 
 set +x
 
-# dev, staging or prod
+# dev or prod
 STACK=${1}
 
+# prod, staging or tst
+INSTANCE_ALIAS=${2}
+
 # Folder containing source code
-SRC_PATH=${2}
+SRC_PATH=${3}
 
 cd $SRC_PATH
 
 mvn clean install
 
-export CMD_PROPS=" -Dorg.sagebionetworks.stack=${STACK}"
+export CMD_PROPS=" -Dorg.sagebionetworks.stack=${STACK} -Dorg.sagebionetworks.org.instance=${INSTANCE_ALIAS}"
 
 java $CMD_PROPS -Xms256m -Xmx2g -cp ./target/stack-builder-0.2.0-SNAPSHOT-jar-with-dependencies.jar org.sagebionetworks.template.cdn.webacl.CdnWebAclBuilderMain
